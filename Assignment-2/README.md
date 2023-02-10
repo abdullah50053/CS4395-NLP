3777721c100932f80c7d8137815d74a6018375ae
Abdullah H <71054105+abdullah50053@users.noreply.github.com>
2023-02-10 01:07:21 -0600

    Update README

---
+++ Assignment-2/README.md
@@ -0,0 +1,7 @@
+# Assignment 2
+
+## Overview
+
+This program reads in a TXT file with a textbook page excerpt and creates a word-guessing game using the top 50 most commonly occurring words.  
+
+To run this program, simply set the argv value to the file path of the CSV data file, and run the program. Follow the prompts to play the game!
--- Assignment-2/wordguesser.py
+++ Assignment-2/wordguesser.py
@@ -1,3 +1,17 @@
 # Abdullah Hasani
 # ahh190004
 # NLP HW 2
+
+# assumes sysarg is /anat19.txt
+import sys
+
+def readfile(filepath):
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('No sysarg provided!')
+    else:
+        filepath = sys.argv[1]
+        print("The file path is " + filepath)
+        contents = readfile(filepath)
+
