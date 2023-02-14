2f353423536fbc304ab67681e3e9c0037252a3c5
Abdullah H <71054105+abdullah50053@users.noreply.github.com>
2023-02-13 23:48:14 -0600

    Fix formatting

--- Assignment-2/wordguesser.py
+++ Assignment-2/wordguesser.py
@@ -11,10 +11,10 @@ import re
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from random import randint
 
 
 def read_file(filepath):
+    # Read the input file as raw text
     with open(os.path.join(os.getcwd(), filepath), 'r') as f:
         text_in = f.read()
     print("\nThe current file reads:")
@@ -23,41 +23,73 @@ def read_file(filepath):
 
 def calculate_lexical_diversity(text):
     nltk.download('popular')
+
+    # Calculate the lexical diversity of the tokenized text
     tokens = word_tokenize(text)
     token_set = set(tokens)
     lexical_diversity = len(token_set)/len(tokens)
+
+    # output lexical diversity
     print("Lexical Deversity: %.2f" % lexical_diversity)
 
 
 def preprocess(text):
+    # reduce the tokens to only those that are alpha
     clean_text = re.sub(r'[.?!,:;()\-\n\d]', ' ', text.lower())
+
+    # tokenize the lower-case raw text
     tokens = word_tokenize(clean_text)
+
+    # reduce to words not in the NLTK stopword list and have length > 5
     stop_words = stopwords.words('english')
     new_tokens = [t for t in tokens if t not in stop_words and len(t) > 5]
+
+    # lemmatize the tokens
     wnl = WordNetLemmatizer()
     lemmas = [wnl.lemmatize(t) for t in new_tokens]
+
+    # use set() to make a list of unique lemmas
     lemmas = list(set(lemmas))
+
+    # do pos tagging on the unique lemmas
     tags = nltk.pos_tag(lemmas)
     ftwenty = tags[:20]
+
+    #  print the first 20 tagged
     print("First 20 tags: ")
     print(ftwenty)
+
+    # create a list of only those lemmas that are nouns
     noun_list = ['NN', 'NNS', 'NNP', 'NNPS']
     nouns = [t for t in tags if t[1] in noun_list]
+
+    # print the number of tokens
     print('Number of tokens: ' + str(len(new_tokens)))
+
+    # print the number of nouns
     print('Number of nouns:' + str(len(nouns)))
+
+    # return tokens and nouns
     return new_tokens, nouns
 
 
 def make_dict(tokens, nouns):
+    # Make a dictionary of {noun:count of noun in tokens} items from the nouns and tokens lists
     new_dict = {}
     new_nouns = [i[0] for i in nouns if i[0] in tokens]
     for i in new_nouns:
         new_dict[i] = tokens.count(i)
+
+    # Sort the dict by count
     sort_dict = dict(sorted(new_dict.items(), key=lambda item: item[1]))
+
+    # Save top 50 words to a list
     top_50 = []
     for i in reversed(sort_dict):
         top_50.append(i)
     top_50 = top_50[:50]
+
+    # Print the 50 most common words and their counts
     print("Top 50 most common words:")
     for i in top_50:
         print(i)
@@ -65,8 +97,11 @@ def make_dict(tokens, nouns):
 
 
 def guessing_game(t50):
+    # Give the user 5 points to start with
     user_pts = 5
     print(f"The game has begun! You have {user_pts} points.")
+
+    # Randomly choose one of the 50 words in the top 50 list
     word = random.choice(t50)
     guesses = []
 
@@ -74,34 +109,50 @@ def guessing_game(t50):
         wrong = 0
         for i in word:
             if i in guesses:
+                # fill in all matching letter _ with the letter
                 print(i, end=' ')
             else:
+                # output to console an “underscore space” for each letter in the word
                 print('_', end=' ')
                 wrong += 1
 
+        # end game if user guesses the answer
         if wrong == 0:
             print("\nYou got it! The word was: " + word)
             break
 
+        # ask the user for a letter
         guess = input("\nGuess a letter: ")
         guesses.append(guess)
 
         if guess not in word:
+            # If the letter is not in the word
             print('Sorry, guess again.')
+            print(f'You have {user_pts} points')
+
+            # Subtract 1 from their score
             user_pts -= 1
+
         elif guess == '!':
+            # If user guesses '!' end game
             print("Game Quit")
             exit(0)
+
         else:
+            # If the letter is in the word
             print('Right!')
+            print(f'You have {user_pts} points')
+
+            # Add 1 point to their score
             user_pts += 1
 
         if user_pts == 0:
             print("\nYou lost! The word was " + word)
 
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('No sysarg provided!')
+        print('No sysarg provided!')  # print an error message and exit the program if no sysarg provided
     else:
         filepath = sys.argv[1]
         print("The file path is " + filepath)
