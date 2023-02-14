249fdf49ab37ab21d6474ae223bf89ff46965d86
Abdullah H <71054105+abdullah50053@users.noreply.github.com>
2023-02-13 20:15:22 -0600

    More preprocessing

--- Assignment-2/wordguesser.py
+++ Assignment-2/wordguesser.py
@@ -18,12 +18,14 @@ def read_file(filepath):
     print("\nThe current file reads:")
     return text_in
 
+
 def calculate_lexical_diversity(text):
     nltk.download('popular')
     tokens = word_tokenize(text)
     token_set = set(tokens)
     lexical_diversity = len(token_set)/len(tokens)
-    print("Lexical Deversity: %.2f" % (lexical_diversity))
+    print("Lexical Deversity: %.2f" % lexical_diversity)
+
 
 def preprocess(text):
     clean_text = re.sub(r'[.?!,:;()\-\n\d]', ' ', text.lower())
@@ -32,8 +34,18 @@ def preprocess(text):
     new_tokens = [t for t in tokens if t not in stop_words and len(t) > 5]
     wnl = WordNetLemmatizer()
     lemmas = [wnl.lemmatize(t) for t in new_tokens]
-    lemmas = set(lemmas)
-    print(lemmas)
+    lemmas = list(set(lemmas))
+    tags = nltk.pos_tag(lemmas)
+    ftwenty = tags[:20]
+    print("First 20 tags: ")
+    print(ftwenty)
+    noun_list = ['NN', 'NNS', 'NNP', 'NNPS']
+    nouns = [t for t in tags if t[1] in noun_list]
+    # print(nouns)
+    print('Number of tokens: ' + str(len(new_tokens)))
+    print('Number of nouns:' + str(len(nouns)))
+    return new_tokens, nouns
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -43,5 +55,5 @@ if __name__ == '__main__':
         print("The file path is " + filepath)
         contents = read_file(filepath)
         calculate_lexical_diversity(contents)
-        preprocess(contents)
+        tokens, nouns = preprocess(contents)
 
